56c3a14d73a74c1879d5ea4ab2f337e5ae83ccf0
StrickVax <strickvax@gmail.com>
2023-07-21 21:05:56 -0700

    Add ProductDetail and placeholder text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
 import './assets/App.css';
+
 
 function App() {
   return (
     <div className='App'>
       <ProductList />
+      <ProductDetail />
     </div>
   );
 }
