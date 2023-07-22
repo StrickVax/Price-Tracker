28aecba40ff09d73f0d3024e8e8766163d0d3141
StrickVax <strickvax@gmail.com>
2023-07-21 23:10:53 -0700

    Add placeholder products and useState

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ProductList() {
+    const [products, setProducts] = useState([
+        { id: 1, name: 'Product 1', price: 10.99, store: 'Store 1' },
+        { id: 2, name: 'Product 2', price: 20.99, store: 'Store 2' },
+    ]);
+
     return (
         <div>
-            <h1>Price Tracker</h1>
+            <h1>Product List</h1>
+            {products.map(product => (
+                <div key={product.id}>
+                    <h2>{product.name}</h2>
+                    <p>{product.price}</p>
+                    <p>{product.store}</p>
+                </div>
+            ))}
         </div>
     );
 }
