4efcdcccb08cfc36cd9f007e125e4c299713ae29
StrickVax <strickvax@gmail.com>
2023-07-22 22:16:24 -0700

    Add SQLite3 server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const { Sequelize, DataTypes, Model } = require('sequelize');
 
-app.get('/',(req, res) => {
-    res.send('Hello World!');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Configure SQLite database
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite',
 });
 
-app.listen(PORT, () => {
-    console.log('Server is running on https://localhost:${PORT}')
+// Define a "Product" model
+class Product extends Model { }
+
+Product.init({
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER
+}, {
+    sequelize,
+    modelName: 'Product'
+});
+
+sequelize.sync();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
