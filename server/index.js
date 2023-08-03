181ab8d85fb0ec49b4ae2ab309c196845d60cd33
StrickVax <strickvax@gmail.com>
2023-08-03 00:22:05 -0700

    Add cors; letting items be added and saved

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+const cors = require('cors');
 const express = require('express');
 const { Sequelize, DataTypes, Model } = require('sequelize');
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 // Configure SQLite database
@@ -27,6 +29,8 @@ app.get('/', (req, res) => {
     res.send('Welcome to my Price Tracker API!');
 });
 
+app.use(express.json());
+
 app.get('/products', async (req, res) => {
     try {
         const products = await Product.findAll();
@@ -35,8 +39,6 @@ app.get('/products', async (req, res) => {
         res.status(500).json({ error: err.message })
     }
 });
-
-app.use(express.json());
 
 
 app.post('/products', async (req, res) => {
